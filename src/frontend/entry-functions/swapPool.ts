8cd7841251de1e62a0a17d95d564b4c0032bb1f8
phatln <phat@undercurrent.tech>
2026-01-30 11:57:38 +0700

    update views and sor

--- src/frontend/entry-functions/swapPool.ts
+++ src/frontend/entry-functions/swapPool.ts
@@ -1,16 +1,48 @@
 import { InputTransactionData } from "@aptos-labs/wallet-adapter-react";
-import { VETAPP_ACCOUNT_ADDRESS } from "@/constants";
+import { TAPP_ACCOUNT_ADDRESS } from "@/constants";
+import { PoolMeta } from "@/hooks/usePool";
+import { AccountAddress, Serializer } from "@aptos-labs/ts-sdk";
 
 export type SwapPoolArguments = {
-  poolAddress: string;
+  poolMeta: PoolMeta,
 };
 
 export const swapPool = (args: SwapPoolArguments): InputTransactionData => {
-  const { poolAddress } = args;
+  const serializer = new Serializer();
+
+  if (args.poolMeta.hook_type == 2) {
+    serializer.serialize(AccountAddress.from(args.poolMeta.pool_addr));
+    serializer.serializeBool(true);
+    serializer.serializeBool(true);
+    serializer.serializeU64(1_000_000n);
+    serializer.serializeU64(0);
+  }
+
+  if (args.poolMeta.hook_type == 3) {
+    serializer.serialize(AccountAddress.from(args.poolMeta.pool_addr));
+    serializer.serializeBool(true); // 0 -> 1
+    serializer.serializeBool(true); 
+    serializer.serializeU64(1_000_000n);
+    serializer.serializeU64(0);
+    serializer.serializeU128(4295048016n); // 0->1
+    // serializer.serializeU128(79226673515401279992447579055n); // 1->0
+  }
+
+  if (args.poolMeta.hook_type == 4) {
+    serializer.serialize(AccountAddress.from(args.poolMeta.pool_addr));
+    serializer.serializeU64(0n);
+    serializer.serializeU64(1n);
+    serializer.serializeU64(1_000_000n);
+    serializer.serializeU64(0);
+  }
+
+  const data = serializer.toUint8Array();
   return {
     data: {
-      function: `${VETAPP_ACCOUNT_ADDRESS}::helper_ve::swaps_pool`,
-      functionArguments: [poolAddress],
+      function: `${TAPP_ACCOUNT_ADDRESS}::router::swap`,
+      functionArguments: [
+        data
+      ],
     },
   };
 };
