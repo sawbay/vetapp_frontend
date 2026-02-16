69f2dae0d60fb8f38088b74cc2397cd4b37ca17f
phatln <phat@undercurrent.tech>
2026-02-16 10:47:38 +0700

    put back dev

--- src/frontend/entry-functions/mintTapp.ts
+++ src/frontend/entry-functions/mintTapp.ts
@@ -1,11 +1,16 @@
 import { InputTransactionData } from "@aptos-labs/wallet-adapter-react";
 import { VETAPP_ACCOUNT_ADDRESS } from "@/constants";
+import { AccountAddress, U64 } from "@aptos-labs/ts-sdk";
 
-export const mintTapp = (): InputTransactionData => {
+export const mintTapp = (receiver: AccountAddress): InputTransactionData => {
+  let args: any[] = [
+    [receiver],
+    [new U64(1000_00_000_000n)]
+  ];
   return {
     data: {
-      function: `${VETAPP_ACCOUNT_ADDRESS}::helper_ve::mint_tapp`,
-      functionArguments: [],
+      function: `${VETAPP_ACCOUNT_ADDRESS}::airdrop::distribute`,
+      functionArguments: args,
     },
   };
 };
